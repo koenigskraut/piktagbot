5768eedd34ab7c5ea692ac1d411a8ef24697fd38
koenigskraut <87582318+koenigskraut@users.noreply.github.com>
2023-08-02 22:17:01 +0300

    quick fix

--- database/util.go
+++ database/util.go
@@ -4,7 +4,7 @@ func StickerTagsUnique(in []*StickerTag) (out []*StickerTag) {
 	out = make([]*StickerTag, 0, len(in))
 	checkUnique := make(map[uint64]bool)
 	for _, st := range in {
-		if checkUnique[st.StickerID] {
+		if !checkUnique[st.StickerID] {
 			out = append(out, st)
 			checkUnique[st.StickerID] = true
 		}
