ee145e28ddd899864dd96ddbd1bc472e5295e446
koenigskraut <87582318+koenigskraut@users.noreply.github.com>
2023-08-07 06:34:50 +0300

    rename files, move fields implementations to separate file

--- webapp/basic_fields.go
+++ webapp/basic_fields.go
@@ -1,41 +1,6 @@
 package webapp
 
-import (
-	"bufio"
-	"errors"
-	"fmt"
-	"sync"
-)
-
-type InitDataField interface {
-	Name() string
-	EncodeData(*bufio.Writer) error
-	DecodeData(*bufio.Reader) error
-}
-
-func EncodeField(w *bufio.Writer, field InitDataField) error {
-	_, err := fmt.Fprintf(w, "%s=", field.Name())
-	if err != nil {
-		return err
-	}
-	return field.EncodeData(w)
-}
-
-func DecodeField(r *bufio.Reader) (InitDataField, error) {
-	name, err := readName(r)
-	if err != nil {
-		return nil, err
-	}
-	fieldFunc, ok := getMapTypes()[name]
-	if !ok {
-		return nil, errors.New(fmt.Sprintf("unknown field name: \"%s\"", name))
-	}
-	field := fieldFunc()
-	if err := field.DecodeData(r); err != nil {
-		return nil, err
-	}
-	return field, nil
-}
+import "bufio"
 
 // QueryID is a unique identifier for the Web App session, required for sending messages via the
 // messages.sendWebViewResultMessage¹ method.
@@ -135,20 +100,4 @@ func (h *Hash) DecodeData(r *bufio.Reader) error {
 	}
 	h.Data = s
 	return nil
-}
-
-var mapTypes map[string]func() InitDataField
-var mapTypesOnce = sync.Once{}
-
-func getMapTypes() map[string]func() InitDataField {
-	mapTypesOnce.Do(func() {
-		mapTypes = map[string]func() InitDataField{
-			QueryIDName:  func() InitDataField { return &QueryID{} },
-			AuthDateName: func() InitDataField { return &AuthDate{} },
-			PrefixName:   func() InitDataField { return &Prefix{} },
-			UserName:     func() InitDataField { return &User{} },
-			HashName:     func() InitDataField { return &Hash{} },
-		}
-	})
-	return mapTypes
 }
